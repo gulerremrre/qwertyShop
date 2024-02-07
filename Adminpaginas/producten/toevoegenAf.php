169e2ddee7f7b89b2cf160a1a812dfd80bf1917c
Yharnam <demirogullariraif@gmail.com>
2024-02-07 14:28:23 +0100

    productenToevoegen_aangepast
    select genre is nu <?php echo $genre?>
    select merk is nu <?php echo $merk ?>

--- Adminpaginas/producten/toevoegenAf.php
+++ Adminpaginas/producten/toevoegenAf.php
@@ -2,6 +2,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
     <meta charset="utf-8">
@@ -27,7 +28,7 @@
     <link rel="stylesheet" href="../../assets/css/update.css">
 
     <script src="wijzigenCheckProduct.js"></script>
-    
+
 
     <style>
         .fout {
@@ -37,55 +38,48 @@
 
 
 
-<?php
-        print_r($_POST);
-        $mysqli = new MySQLi("localhost", "root", "", "computershopphp");
-         $Error = "";
+    <?php
+    print_r($_POST);
+    $mysqli = new MySQLi("localhost", "root", "", "computershopphp");
+    $Error = "";
 
-        if ((isset($_POST['productnaamtoe'])) && ($_POST['productnaamtoe']) != "")
-        {
-             
-            $sql = "INSERT INTO tblartikels (artikelnaam, prijs, korting, genreid, omschrijving, merk) VALUES (?, ?, ?, ?, ?, ?)";
-            $stmt = $mysqli->prepare($sql);
-        
-            $stmt->bind_param('sdisss', $artikelNaam, $prijs, $korting, $genreid, $omschrijving, $merk);
-            $artikelNaam = $_POST['productnaamtoe'];
-            $korting = $_POST['productkortingtoe'];
-            $prijs = $_POST["prijstoe"];
-            $merk = $_POST['merktoe'];
-            $omschrijving = $_POST['omschrijvingtoe'];
-            $genreid = $_POST['genretoe'];
+    if ((isset($_POST['productnaamtoe'])) && ($_POST['productnaamtoe']) != "") {
+
+        $sql = "INSERT INTO tblartikels (artikelnaam, prijs, korting, genreid, omschrijving, merk) VALUES (?, ?, ?, ?, ?, ?)";
+        $stmt = $mysqli->prepare($sql);
+
+        $stmt->bind_param('sdisss', $artikelNaam, $prijs, $korting, $genreid, $omschrijving, $merk);
+        $artikelNaam = $_POST['productnaamtoe'];
+        $korting = $_POST['productkortingtoe'];
+        $prijs = $_POST["prijstoe"];
+        $merk = $_POST['merktoe'];
+        $omschrijving = $_POST['omschrijvingtoe'];
+        $genreid = $_POST['genretoe'];
 
 
-            if ($stmt->execute())
-            {
-                    header("location:alleProducten.php");
-            } 
-            else 
-            {
-                    echo '<p class="text-danger">Error: ' . $stmt->error . '</p>';
-            }
-        
-
-            $stmt->close();
-
-        } 
-        else 
-        {
-                $Error = '<p class="text-danger">Vul alle velden in.</p>';
-                echo $Error;
+        if ($stmt->execute()) {
+            header("location:alleProducten.php");
+        } else {
+            echo '<p class="text-danger">Error: ' . $stmt->error . '</p>';
         }
-        
+
+
+        $stmt->close();
+    } else {
+        $Error = '<p class="text-danger">Vul alle velden in.</p>';
+        echo $Error;
+    }
+
     ?>
 
 <body>
-<header class="header-area header-sticky">
+    <header class="header-area header-sticky">
         <div class="container">
             <div class="row">
                 <div class="col-12">
                     <nav class="main-nav">
                         <ul class="nav">
-                            <li class="scroll-to-section"><a href="../../index.html" >Home</a></li>
+                            <li class="scroll-to-section"><a href="../../index.html">Home</a></li>
                             <li class="scroll-to-section"><a href="../../gamingpc.html">Pc's</a></li>
                             <li class="scroll-to-section"><a href="../../gaminglaptop.html">Laptops</a></li>
 
@@ -102,7 +96,7 @@
                             </li>
                             <li class="scroll-to-section"><a href="../../login.html">login</a></li>
                             <li class="scroll-to-section"><a href="../adminMain.html">Admin</a></li>
-                        </ul>        
+                        </ul>
                         <a class='menu-trigger'>
                             <span>Menu</span>
                         </a>
@@ -114,129 +108,118 @@
     </header>
 
 
-<!-- form -->
-   
-   <div class="container mt-5 ">
-    <div class="row">
-        <div class="col-md-6 mx-auto">
-            <br>
-            <h2 class="mt-5">Toevoegen</h2>
-            <br>
-            <form name="formtoevoegen" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" class="bg-info bg-info  rounded p-3">
-                <div class="form-group">
-                    <label for="productnaamtoe">ArtikelNaam:</label>
-                    <input type="text" class="form-control" id="productnaamtoe" required name="productnaamtoe">
-                    <label id="productongeldig" class="fout"></label>
-                </div>
-                <div class="form-group">
-                    <label for="prijstoe">Prijs:</label>
-                    <input type="text" class="form-control" id="prijstoe" required name="prijstoe">
-                    <label id="prijsongeldig" class="fout"></label>
-                </div>
-                <div class="form-group">
-                    <label for="productkortingtoe">Korting:</label>
-                    <input type="text" class="form-control" id="productkortingtoe" name="productkortingtoe">
-                    <label id="kortingongeldig" class="fout"></label>
-                </div>
-                <div class="form-group">
-                    <label for="merktoe">Merk:</label>
-                    <select name="merktoe" id="merktoe" class="form-control">
-                            <option value="" selected>Select Merk</option>
+    <!-- form -->
+
+    <div class="container mt-5 ">
+        <div class="row">
+            <div class="col-md-6 mx-auto">
+                <br>
+                <h2 class="mt-5">Toevoegen</h2>
+                <br>
+                <form name="formtoevoegen" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>" class="bg-info bg-info  rounded p-3">
+                    <div class="form-group">
+                        <label for="productnaamtoe">ArtikelNaam:</label>
+                        <input type="text" class="form-control" id="productnaamtoe" required name="productnaamtoe">
+                        <label id="productongeldig" class="fout"></label>
+                    </div>
+                    <div class="form-group">
+                        <label for="prijstoe">Prijs:</label>
+                        <input type="text" class="form-control" id="prijstoe" required name="prijstoe">
+                        <label id="prijsongeldig" class="fout"></label>
+                    </div>
+                    <div class="form-group">
+                        <label for="productkortingtoe">Korting:</label>
+                        <input type="text" class="form-control" id="productkortingtoe" name="productkortingtoe">
+                        <label id="kortingongeldig" class="fout"></label>
+                    </div>
+                    <div class="form-group">
+                        <label for="merktoe">Merk:</label>
+                        <select name="merktoe" id="merktoe" class="form-control">
+                            <option value="" selected> <?php echo $merk ?></option>
                             <?php
                             $sql = "SELECT merk FROM tblmerk ORDER BY merk ASC ";
 
-                            if ($stmt = $mysqli->prepare($sql))
-                            {
-                                if (!$stmt->execute())
-                                {
+                            if ($stmt = $mysqli->prepare($sql)) {
+                                if (!$stmt->execute()) {
                                     echo 'Het uitvoeren van de query is mislukt: ' . $stmt->error . ' in query: ' . $sql;
                                 } else {
                                     $stmt->bind_result($merk);
-                                    while ($stmt->fetch())
-                                    {
+                                    while ($stmt->fetch()) {
 
                                         echo '<option>' . $merk .  '</option>';
                                     }
                                 }
                                 $stmt->close();
-                            } 
-                            else
-                            {
+                            } else {
                                 echo 'Er zit een fout in de query: ' . $mysqli->error;
                             }
 
                             ?>
-                    </select>
-                    <label id="merkongeldig" class="fout"></label>
-                </div>
-                <div class="form-group">
-                    <label for="omschrijvingtoe">Omschrijving:</label>
-                    <input type="text" class="form-control" id="omschrijvingtoe" name="omschrijvingtoe">
-                    <label id="omschrijvingongeldig" class="fout"></label>
-                </div>
-                <div class="form-group">
-                    <label for="genretoe">Genre:</label>
-                    <select name="genretoe" id="genretoe" class="form-control">
-                            <option value="" selected>Select Genre</option>
+                        </select>
+                        <label id="merkongeldig" class="fout"></label>
+                    </div>
+                    <div class="form-group">
+                        <label for="omschrijvingtoe">Omschrijving:</label>
+                        <input type="text" class="form-control" id="omschrijvingtoe" name="omschrijvingtoe">
+                        <label id="omschrijvingongeldig" class="fout"></label>
+                    </div>
+                    <div class="form-group">
+                        <label for="genretoe">Genre:</label>
+                        <select name="genretoe" id="genretoe" class="form-control">
+                            <option value="" selected><?php echo $genre2 ?></option>
                             <?php
-                                $sql = "SELECT genre FROM tblgenre ORDER BY genre ASC";
+                            $sql = "SELECT genre FROM tblgenre ORDER BY genre ASC";
 
-                                if ($stmt = $mysqli->prepare($sql))
-                                {
-                                    if (!$stmt->execute())
-                                    {
-                                        echo 'Het uitvoeren van de query is mislukt: ' . $stmt->error . ' in query: ' . $sql;
-                                    } 
-                                    else
-                                    {
-                                        $stmt->bind_result($genre2);
-                                        while ($stmt->fetch())
-                                        {
-                                            echo '<option value="' . $genre2 . '">' . $genre2 .  '</option>';
-                                        }
+                            if ($stmt = $mysqli->prepare($sql)) {
+                                if (!$stmt->execute()) {
+                                    echo 'Het uitvoeren van de query is mislukt: ' . $stmt->error . ' in query: ' . $sql;
+                                } else {
+                                    $stmt->bind_result($genre2);
+                                    while ($stmt->fetch()) {
+                                        echo '<option value="' . $genre2 . '">' . $genre2 .  '</option>';
                                     }
-                                    $stmt->close();
-                                } 
-                                else
-                                {
-                                    echo 'Er zit een fout in de query: ' . $mysqli->error;
                                 }
+                                $stmt->close();
+                            } else {
+                                echo 'Er zit een fout in de query: ' . $mysqli->error;
+                            }
                             ?>
-                        
-                    </select>
-                    <label id="genreongeldig" class="fout"></label>
-                </div>
+
+                        </select>
+                        <label id="genreongeldig" class="fout"></label>
+                    </div>
                     <input type="button" name="voegtoe" id="voegToe" value="voegtoe" class="btn btn-secondary" onclick="wijzig()">
-            </form>
-            <br>
+                </form>
+                <br>
+            </div>
         </div>
-    </div>
 
-<!-- form -->
+        <!-- form -->
 
 
- <!-- jQuery -->
- <script src="../../assets/js/jquery-2.1.0.min.js"></script>
+        <!-- jQuery -->
+        <script src="../../assets/js/jquery-2.1.0.min.js"></script>
 
-<!-- Bootstrap -->
-<script src="../../assets/js/popper.js"></script>
-<script src="../../assets/js/bootstrap.min.js"></script>
+        <!-- Bootstrap -->
+        <script src="../../assets/js/popper.js"></script>
+        <script src="../../assets/js/bootstrap.min.js"></script>
 
-<!-- Plugins -->
-<script src="../../assets/js/owl-carousel.js"></script>
-<script src="../../assets/js/accordions.js"></script>
-<script src="../../assets/js/datepicker.js"></script>
-<script src="../../assets/js/scrollreveal.min.js"></script>
-<script src="../../assets/js/waypoints.min.js"></script>
-<script src="../../assets/js/jquery.counterup.min.js"></script>
-<script src="../../assets/js/imgfix.min.js"></script>
-<script src="../../assets/js/slick.js"></script>
-<script src="../../assets/js/lightbox.js"></script>
-<script src="../../assets/js/isotope.js"></script>
+        <!-- Plugins -->
+        <script src="../../assets/js/owl-carousel.js"></script>
+        <script src="../../assets/js/accordions.js"></script>
+        <script src="../../assets/js/datepicker.js"></script>
+        <script src="../../assets/js/scrollreveal.min.js"></script>
+        <script src="../../assets/js/waypoints.min.js"></script>
+        <script src="../../assets/js/jquery.counterup.min.js"></script>
+        <script src="../../assets/js/imgfix.min.js"></script>
+        <script src="../../assets/js/slick.js"></script>
+        <script src="../../assets/js/lightbox.js"></script>
+        <script src="../../assets/js/isotope.js"></script>
 
-<!-- Global Init -->
-<script src="../../assets/js/custom.js"></script>
+        <!-- Global Init -->
+        <script src="../../assets/js/custom.js"></script>
 
 
 </body>
+
 </html>
